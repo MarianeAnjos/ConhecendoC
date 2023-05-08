b3cf1d0ad697e781ce12ffac2af237e371d3486f
Mariane Anjos <100323487+MarianeAnjos@users.noreply.github.com>
2023-05-08 19:38:14 -0300

    Variaveis, entrada e saida de dados

---
+++ Entrada e saída de dados.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class Variaveis 
+
+{ 
+        static void Main()
+        {
+            Console.WriteLine("Qual seu nome?");
+            String nome = Console.ReadLine();
+            Console.WriteLine("Quantos aos você tem?"); 
+            int idade - int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Seu nome é {0} e sua idade é de {1}", nome, idade);
+        }
+
+}

dbb145b59d2299e5edf0f0ee5a49eee1a513d1a7
Mariane Anjos <100323487+MarianeAnjos@users.noreply.github.com>
2023-05-08 18:58:48 -0300

    Meu primeiro Print utilizando c#

---
+++ OlaMundo.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*Este é um comentario na linha em C#*/
+
+Console.WriteLine(" Este é o meu primeiro Print utilizando C#");

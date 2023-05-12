fee1f77c686a41170d633bc3d482f834685c0fee
Mariane Anjos <100323487+MarianeAnjos@users.noreply.github.com>
2023-05-12 15:57:13 -0300

    Operadores true e false

---
+++ Operadores logicos.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+interface OperadoresLogicos { 
+    static void Main(String[] args)
+    {
+        var atividade1 = true;
+        var atividade2 = false;
+
+        var todas = atividade1 && atividade2;
+
+        Console.WriteLine("Fez todas as atividades? {0}", todas);
+    }
+}

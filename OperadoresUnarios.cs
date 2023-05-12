85a03fa8724dcc4f5ae15e7e4806a9e98b3670d5
Mariane Anjos <100323487+MarianeAnjos@users.noreply.github.com>
2023-05-12 16:19:38 -0300

    Operadores Unarios

---
+++ OperadoresUnarios.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+interface OperadoresUnarios { 
+    public void Main(String[] args)
+    {
+        var x = false;
+        var n1 = 6;
+        var n2 = 5;
+        var n3 = 7;
+        var n4 = 8;
+
+        Console.WriteLine(!x);
+        n3++;
+        Console.WriteLine(n3);
+        n4++;
+        Console.WriteLine(n4);
+
+        Console.WriteLine(n2 == --n1);
+        Console.WriteLine(n2-- == ++n1);
+
+    }
+}

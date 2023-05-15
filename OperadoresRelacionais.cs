719ed54c5bc6374986e42cfb76bb54ee4a9a2c24
Mariane Anjos <100323487+MarianeAnjos@users.noreply.github.com>
2023-05-15 09:35:31 -0300

    Operadores relacionais em c#

---
+++ OperadoresRelacionais.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+interface OrepadoresRelacionais2
+{
+       public void Main(string[] args){
+
+        Console.WriteLine("Digite a sua nota: ");
+        double.TryParse(Console.ReadLine(), out double nota);
+
+        Console.WriteLine("Nota maior que 7? {0}", nota > 7.0);
+        Console.WriteLine("Tirou 10?", nota == 10.0);
+
+    }
+}
